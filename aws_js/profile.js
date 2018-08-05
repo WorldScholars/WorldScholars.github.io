413df3a5881d7a0f84159e9884e07729bfb465dc
Mark Santolucito <mark.santolucito@yale.edu>
2018-08-05 07:37:43 -0400

    autocommit

--- aws_js/profile.js
+++ aws_js/profile.js
@@ -18,7 +18,47 @@ WorldScholars.map = WorldScholars.map || {};
         window.location.href = '/signin.html';
     });
 
+    function listExams() {
+
+        $.ajax({
+            method: 'GET',
+            url: _config.api.invokeUrl + '/examlist',
+            headers: {
+                Authorization: authToken
+            },
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error reading exams from database: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when reading your exam\n' + jqXHR.responseText);
+            }
+        });
+    }
+
+    function listExamsInHtml(allExams) {
+        console.log(allExams);
+        allExams.forEach(function(oneExam){
+          //TODO hardcoding parsing of section and question, this should just be in the json object
+          slash = "/";
+          if (ans.value!="0.00"){
+            newRow = ('<tr><td>' + oneExam.ExamNumber + '<'+slash+'td><td>' + oneExam.submitTime + '<'+slash+'td>'+
+                '<td> <a href=\"/examsummary.html?id=' + oneExam.ExamId + '\">Results<'+slash+'</a>'+'<'+slash+'td><'+slash+'tr>');
+            $('tbody').append(newRow);
+          }
+        });
+
+
+    }
+
     $(function onDocReady() {
+        //TODO figure out how to split this into multiple files 
+        if (window.location.pathname=="/profile.html"){
+          var useremail = document.getElementById('username');
+          listExams()
+          useremail.innerHTML = WorldScholars.user.username;
+        }
+
         // register handler for signout click
         $('#signOut').click(function() {
             WorldScholars.signOut();
@@ -26,11 +66,6 @@ WorldScholars.map = WorldScholars.map || {};
             window.location = "signin.html";
         });
   
-        //TODO figure out how to split this into multiple files 
-        if (window.location.pathname=="/profile.html"){
-          var useremail = document.getElementById('username');
-          useremail.innerHTML = WorldScholars.user.username;
-        }
         
     });
 
